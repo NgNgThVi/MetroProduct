918a7eb958ba7d6a85dec43ba8f5becabacb9bfc
chim347 <nhanntse160792@gmail.com>
2023-10-19 20:16:16 +0700

    8h16 19/10/2023

--- Backend/Infrastructure/MetroDelivery.Identity/Configurations/EntityConfiguration/Store_MenuConfiguration.cs
+++ Backend/Infrastructure/MetroDelivery.Identity/Configurations/EntityConfiguration/Store_MenuConfiguration.cs
@@ -39,6 +39,12 @@ namespace MetroDelivery.Identity.Configurations.EntityConfiguration
                     StoreId = Guid.Parse("2F3EEE35-1B8E-43AF-956F-EACD94EEA7CD"),
                     MenuId = Guid.Parse("E7A15238-5DB0-49BD-94E6-D5F8B77AE6CE")
                 }
+                ,
+                new Store_Menu
+                {
+                    StoreId = Guid.Parse("2f3eee35-1b8e-43af-956f-eacd94eea7cd"),
+                    MenuId = Guid.Parse("5263453c-dae9-45e4-8204-5430a7256cde")
+                }
             );
         }
     }
