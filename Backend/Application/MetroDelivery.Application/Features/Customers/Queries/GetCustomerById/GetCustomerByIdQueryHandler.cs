9a8c3100e21341a7bab49b5742a830a00a1f68bf
chim347 <nhanntse160792@gmail.com>
2023-11-03 14:39:11 +0700

    2h39 11/3/2023

--- Backend/Application/MetroDelivery.Application/Features/Customers/Queries/GetCustomerById/GetCustomerByIdQueryHandler.cs
+++ Backend/Application/MetroDelivery.Application/Features/Customers/Queries/GetCustomerById/GetCustomerByIdQueryHandler.cs
@@ -36,9 +36,6 @@ namespace MetroDelivery.Application.Features.Customers.Queries.GetCustomerById
 
             if (user != null && user.EmailConfirmed) {
                 var isEndUser = await _userManager.IsInRoleAsync(user, "EndUser");
-                var isManager = await _userManager.IsInRoleAsync(user, "Manager");
-                var isStaff = await _userManager.IsInRoleAsync(user, "Staff");
-                var isAdmin = await _userManager.IsInRoleAsync(user, "Admin");
                 if (isEndUser) {
                     var customerRole = new CustomerRole
                     {
@@ -64,87 +61,6 @@ namespace MetroDelivery.Application.Features.Customers.Queries.GetCustomerById
                     };
 
                     return customerRole;
-                }
-                if (isManager) {
-                    var staffRole = new CustomerRole
-                    {
-                        CustomerId = user.Id,
-                        CustomerData = new CustomerInfo
-                        {
-                            Id = user.Id,
-                            Email = user.Email,
-                            FirstName = user.FirstName,
-                            LastName = user.LastName,
-                            PhoneNumber = user.PhoneNumber,
-                            Address = user.Address,
-                            Birthday = user.Birthday,
-                            StoreId = user.StoreId,
-                            StoreData = _mapper.Map<StoreData>(_metroPickUpDbContext.Store.Where(s => s.Id == user.StoreId).SingleOrDefault()),
-                            Created = user.Created,
-                        },
-                        RoleId = "04D67210-257D-4DD5-BAFC-13DDE8CA0DFE",
-                        RoleData = new RoleData
-                        {
-                            Id = "04D67210-257D-4DD5-BAFC-13DDE8CA0DFE",  // Thay thế "YourRoleId" bằng Id thực tế của vai trò "Staff" trong bảng AspNetRoles
-                            Name = "Manager"      // Thay thế "Staff" bằng tên thực tế của vai trò "Staff" trong bảng AspNetRoles
-                        }
-                    };
-
-                    return staffRole;
-                }
-                if (isStaff) {
-                    var staffRole = new CustomerRole
-                    {
-                        CustomerId = user.Id,
-                        CustomerData = new CustomerInfo
-                        {
-                            Id = user.Id,
-                            Email = user.Email,
-                            FirstName = user.FirstName,
-                            LastName = user.LastName,
-                            PhoneNumber = user.PhoneNumber,
-                            Address = user.Address,
-                            Birthday = user.Birthday,
-                            StoreId = user.StoreId,
-                            StoreData = _mapper.Map<StoreData>(_metroPickUpDbContext.Store.Where(s => s.Id == user.StoreId).SingleOrDefault()),
-                            Created = user.Created,
-                        },
-                        RoleId = "647D9649-F5A2-4F24-808F-6FC326EC2AA3",
-                        RoleData = new RoleData
-                        {
-                            Id = "647D9649-F5A2-4F24-808F-6FC326EC2AA3",  // Thay thế "YourRoleId" bằng Id thực tế của vai trò "Staff" trong bảng AspNetRoles
-                            Name = "Staff"      // Thay thế "Staff" bằng tên thực tế của vai trò "Staff" trong bảng AspNetRoles
-                        }
-                    };
-
-                    return staffRole;
-                }
-                if (isAdmin) {
-                    var staffRole = new CustomerRole
-                    {
-                        CustomerId = user.Id,
-                        CustomerData = new CustomerInfo
-                        {
-                            Id = user.Id,
-                            Email = user.Email,
-                            FirstName = user.FirstName,
-                            LastName = user.LastName,
-                            PhoneNumber = user.PhoneNumber,
-                            Address = user.Address,
-                            Birthday = user.Birthday,
-                            StoreId = user.StoreId,
-                            StoreData = _mapper.Map<StoreData>(_metroPickUpDbContext.Store.Where(s => s.Id == user.StoreId).SingleOrDefault()),
-                            Created = user.Created,
-                        },
-                        RoleId = "AF5EB4AC-219A-4BC1-99FE-8C23876536EA",
-                        RoleData = new RoleData
-                        {
-                            Id = "AF5EB4AC-219A-4BC1-99FE-8C23876536EA",  // Thay thế "YourRoleId" bằng Id thực tế của vai trò "Staff" trong bảng AspNetRoles
-                            Name = "Admin"      // Thay thế "Staff" bằng tên thực tế của vai trò "Staff" trong bảng AspNetRoles
-                        }
-                    };
-
-                    return staffRole;
                 }
             }
 
---
+++ Backend/MetroDelivery.API/Controllers/Admin/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using MetroDelivery.Application.Features.Customers.Queries.GetCustomerById;
+using MetroDelivery.Application.Features.Customers;
+using Microsoft.AspNetCore.Mvc;
+using MetroDelivery.Application.Features.Customers.Queries.GetByIdForAdmin;
+using MediatR;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MetroDelivery.API.Controllers.Admin
+{
+    [Route("api/v1/admin")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AdminController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("get-all-for-admin-by-all-id")]
+        /*[Authorize(Roles = "Admin, EndUser")]*/
+        // đang get detail cho cả 4 role, đổi tên lại
+        public async Task<ActionResult<CustomerRole>> GetUserById([FromQuery] GetByIdForAdminQuery request)
+        {
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
