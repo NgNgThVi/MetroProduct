52c3fc73f870e431ca25f0983a024ae68821efe1
chim347 <nhanntse160792@gmail.com>
2023-11-06 12:28:50 +0700

    11/6/2023 12h28

--- Backend/MetroDelivery.API/Controllers/Admin/AdminController.cs
+++ Backend/MetroDelivery.API/Controllers/Admin/AdminController.cs
@@ -43,5 +43,16 @@ namespace MetroDelivery.API.Controllers.Admin
             var response = await _mediator.Send(request);
             return Ok(response);
         }
+
+        [HttpGet]
+        [Route("{applicationuserid}")]
+        /*[Authorize(Roles = "Admin, EndUser")]*/
+        // đang get detail cho cả 4 role, đổi tên lại
+        public async Task<ActionResult<CustomerRole>> GetUserById123(string applicationuserid)
+        {
+            var request = new GetByIdForAdminQuery(applicationuserid);
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
     }
 }
